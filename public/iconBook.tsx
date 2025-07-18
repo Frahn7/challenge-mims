647df3644dacc9186641abb982765e14f5f6e084
Francisco Villella <franciscovillella2003@gmail.com>
2025-07-18 20:47:41 -0300

    add icons

---
+++ public/iconBook.tsx
@@ -0,0 +1,34 @@
+export const IconBook = () => {
+  return (
+    <svg
+      width="64"
+      height="64"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5 4C5 3.44772 5.44772 3 6 3H18C18.5523 3 19 3.44772 19 4V18C19 18.5523 18.5523 19 18 19H7C5.89543 19 5 18.1046 5 17V4Z"
+        stroke="#888"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M5 17C5 18.1046 5.89543 19 7 19H18"
+        stroke="#888"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+      />
+      <line
+        x1="9"
+        y1="7.75"
+        x2="15"
+        y2="7.75"
+        stroke="#888"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+};
